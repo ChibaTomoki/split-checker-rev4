edd4938648aaad05f4a6cdbbc184539542c9e440
ChibaTomoki <michiamo.t1@gmail.com>
2022-08-19 01:12:19 +0900

    ざっと必要なコンポーネントのひな型作成

---
+++ frontend/src/components/DeletedList.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+  <div>DeletedList</div>
+</template>
---
+++ frontend/src/components/UnsettledList.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+  <div>UnsettledList</div>
+</template>
